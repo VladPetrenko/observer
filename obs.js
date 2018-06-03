68fe2ac62cd1623a57c71674549f2191c0c67cd0
VladPetrenko <petrenko.vlad1991@gmail.com>
2018-06-03 18:14:17 +0300

    first update

--- obs.js
+++ obs.js
@@ -5,36 +5,64 @@ class Observable {
     this.observers = [];
   }
 
-  subscribe(f) {
-    this.observers.push(f);
+  subscribe(observer) {
+    this.observers.push(observer);
   }
 
-  unsubscribe(f) {
-    this.observers = this.observers.filter(subscriber => subscriber !== f);
+  unsubscribe(observer) {
+    this.observers = this.observers.filter(subscriber => subscriber !== observer);
   }
 
   notify(data) {
     this.observers.forEach(observer => observer(data));
   }
+
+  once() {
+
+  }
+
+  sth() {
+
+  }
 }
 
-const Obs = new Observable();
 
-class Users {
-	constructor(name) {
-		this.userName = name;
-		this.changes = new Observable;
 
-	}
-	
-     notify() {
-      console.log("User " + this.userName + " is notified!");
-    }
+class User {
+  constructor(name) {
+    this.userName = name;
+    this.info = this.post.bind(this);
   }
+
+   post(data) {
+    console.log(`${this.userName} has a news post from Journalist about ${data}`);
+   }
+  }
+
+class Journalist {
+    constructor(name) {
+        this.name = name;
+    }
+    
+    postMessage(data) {
+        return `${data}`;
+    }
+}
+
 	
+let obs = new Observable();
+let verge = new Journalist('Verge');
+let bloomberg = new Journalist('Bloomberg');
+let user1 = new User('John');
+let user2 = new User('Ed');
+let user3 = new User('Tom');
 
-let user1 = new Users('John');
-let user2 = new Users('Ed');
-let user3 = new Users('Tom');
+obs.subscribe(user1.info);
+obs.subscribe(user2.info);
+obs.subscribe(user3.info);
 
+obs.notify(verge.postMessage('yohoo'));
 
+obs.unsubscribe(user3.info);
+
+obs.notify(bloomberg.postMessage('sth new'));
